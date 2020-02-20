e7dc03f05862a37daebfeff2ad46879fc433c833
wlukla <vladislav.danilochkin@gmail.com>
2020-02-20 14:19:37 +0200

    feat: connect shopping-cart-table to redux store

--- src/components/shopping-cart-table/shopping-cart-table.jsx
+++ src/components/shopping-cart-table/shopping-cart-table.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
 import './shopping-cart-table.css';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
-import { faTrash, faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+import { connect } from 'react-redux';
 
-const ShoppingCartTable = () => {
+
+
+const ShoppingCartTable = ({ items, total, onIncrease, onDecrease, onDelete}) => {
+  const renderRow = (item, idx) => {
+    const { id, title, count, total} = item;
+
+    return (
+      <tr key={id}>
+        <td>{idx + 1}</td>
+        <td>{title}</td>
+        <td>{count}</td>
+        <td>{total}</td>
+        <td className="actions">
+          <button
+            className="btn btn-outline-danger btn-sm"
+            onClick={ () => onDelete(id) }
+          >
+            <Icon icon={faTrash} />
+          </button>
+          <button
+            className="btn btn-outline-success btn-sm"
+            onClick={ () => onIncrease(id) }
+          >
+            <Icon icon={faPlusCircle} />
+          </button>
+          <button
+            className="btn btn-outline-warning btn-sm"
+            onClick={ () => onDecrease(id) }
+          >
+            <Icon icon={faMinusCircle} />
+          </button>
+        </td>
+      </tr>
+    )
+  }
+
   return (
     <div className="shopping-cart-table">
       <h2>Your Order</h2>
@@ -13,37 +49,45 @@ const ShoppingCartTable = () => {
             <th>#</th>
             <th>Item</th>
             <th>Count</th>
-            <th>Price</th>
+            <th>Total</th>
             <th>Action</th>
           </tr>
         </thead>
 
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Dick</td>
-            <td>2</td>
-            <td>44</td>
-            <td className="actions">
-              <button className="btn btn-outline-danger btn-sm">
-                <Icon icon={faTrash} />
-              </button>
-              <button className="btn btn-outline-success btn-sm">
-                <Icon icon={faPlusCircle} />
-              </button>
-              <button className="btn btn-outline-warning btn-sm">
-                <Icon icon={faMinusCircle} />
-              </button>
-            </td>
-          </tr>
+          {
+            items.map(renderRow)
+          }
         </tbody>
       </table>
 
       <div className="total">
-        Total: 20000$
+        Total: ${total}
       </div>
     </div>
   )
 }
 
-export default ShoppingCartTable;
+const mapStateToProps = ({ cartItems, orderTotal }) => ({
+  items: cartItems,
+  total: orderTotal,
+})
+
+const mapDispatchToProps = () => {
+  return {
+    onIncrease: (id) => {
+      console.log('increased', {id})
+    },
+    onDecrease: (id) => {
+      console.log('decreased', {id})
+    },
+    onDelete: (id) => {
+      console.log('deleted', {id})
+    },
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ShoppingCartTable);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,24 +2,36 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItems: [
+    {
+      id: 1,
+      title: 'Book1',
+      count: 3,
+      total: 100
+    }
+  ],
+  orderTotal: 10000,
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_BOOKS_REQUEST':
       return {
+        ...state,
         books: [],
         loading: true,
         error: null,
       }
     case 'FETCH_BOOKS_SUCCESS':
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
       };
     case 'FETCH_BOOKS_FAILURE':
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,
